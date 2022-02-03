779e37c8962661bebc319ad71c22de5494ab8502
akshayav08 <akshayav08@gmail.com>
2022-02-02 22:44:52 -0500

    expense-eg

--- react-first/src/App.js
+++ react-first/src/App.js
@@ -1,8 +1,12 @@
+import ExpenseItem from './components/ExpenseItem';
+;
+
 
 function App() {
   return (
     <div>
      Hello World
+     <ExpenseItem></ExpenseItem>
     </div>
   );
 }
---
+++ react-first/src/components/ExpenseItem.js
@@ -0,0 +1,10 @@
+function ExpenseItem(){
+    return(
+        <div>
+            <h2>Expense1</h2>
+            <div>Date</div>
+            <div>amount $30</div>
+        </div>
+    );
+}
+export default ExpenseItem;
